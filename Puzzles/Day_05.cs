e7ffac9274404464e207f38ddfae375c1c5be18a
Paeseitje <tine.paessens@gmail.com>
2022-12-11 11:54:06 +0100

    working solution to Day 5 puzzle 1

--- Puzzles/Day_05.cs
+++ Puzzles/Day_05.cs
@@ -21,7 +21,7 @@ namespace AdventOfCode2022.Puzzles
             var lijst_6 = new List<string>() { "Q", "W", "V", "H", "Z", "R", "G", "B" };
             var lijst_7 = new List<string>() { "V", "J", "P", "C", "B", "D", "N" };
             var lijst_8 = new List<string>() { "P", "T", "B", "Q"};
-            var lijst_9 = new List<string>() { "H", "G", "Z", "R", "c" };
+            var lijst_9 = new List<string>() { "H", "G", "Z", "R", "C" };
 
             var input = LoadDataListAsStringList(5, 1);
 
@@ -29,36 +29,105 @@ namespace AdventOfCode2022.Puzzles
 
             foreach (var order in input)
             {
-                var workorder = string.Concat(order.Where(Char.IsDigit));
                 var numbers = new List<int>();
-                foreach (char number in workorder)
+
+                string[] values = Regex.Split(order, @"\D+");
+                foreach (string value in values)
                 {
-                    numbers.Add((int)char.GetNumericValue(number));
+                    if (!string.IsNullOrEmpty(value))
+                    {
+                        int i = int.Parse(value);
+                        numbers.Add(i);
+                    }
                 }
                 workorderList.Add(numbers);
             }
 
-
             foreach (var order in workorderList)
             {
-
                 // loop elk order na en voor uit wat er moet gebeuren
                 int aantal = order[0];
                 int from_stack = order[1];
                 int to_stack = order[2];
-
-                for (int i = 1; i <= aantal; i++)
+                var box = "";
+                for (int i = aantal; i > 0; i--)
                 {
-                    // take last box from stack en place it on the dedicated stack
-                    // take last box from stack
-                    var take_from_stack = from_stack == 1 ? lijst_1 : from_stack == 2 ? lijst_2 :
-                        from_stack == 3 ? lijst_3 : from_stack == 4 ? lijst_4 : from_stack == 5 ? lijst_5 : from_stack == 6 ? lijst_6 : from_stack == 7 ? lijst_7 : from_stack == 8 ? lijst_8 : lijst_9;
-                    var add_to_stack = to_stack == 1 ? lijst_1 : to_stack == 2 ? lijst_2 :
-                        to_stack == 3 ? lijst_3 : to_stack == 4 ? lijst_4 : to_stack == 5 ? lijst_5 : to_stack == 6 ? lijst_6 : to_stack == 7 ? lijst_7 : to_stack == 8 ? lijst_8 : lijst_9;
+                    switch (from_stack)
+                    {
+                        case 1 :
+                            box = lijst_1.Last();
+                            lijst_1.RemoveAt(lijst_1.Count -1) ;
+                            break;
+                        case 2:
+                            box = lijst_2.Last();
+                            lijst_2.RemoveAt(lijst_2.Count - 1);
+                            break;
+                        case 3:
+                            box = lijst_3.Last();
+                            lijst_3.RemoveAt(lijst_3.Count - 1);
+                            break;
+                        case 4:
+                            box = lijst_4.Last();
+                            lijst_4.RemoveAt(lijst_4.Count - 1);
+                            break;
+                        case 5:
+                            box = lijst_5.Last();
+                            lijst_5.RemoveAt(lijst_5.Count - 1);
+                            break;
+                        case 6:
+                            box = lijst_6.Last();
+                            lijst_6.RemoveAt(lijst_6.Count - 1);
+                            break;
+                        case 7:
+                            box = lijst_7.Last();
+                            lijst_7.RemoveAt(lijst_7.Count - 1);
+                            break;
+                        case 8:
+                            box = lijst_8.Last();
+                            lijst_8.RemoveAt(lijst_8.Count - 1);
+                            break;
+                        case 9:
+                            box = lijst_9.Last();
+                            lijst_9.RemoveAt(lijst_9.Count - 1);
+                            break;
+                        default:
+                            break;
+                    }
 
-                    var box = take_from_stack.Last();
-                    take_from_stack.RemoveAt(take_from_stack.Count -1) ;
-                    add_to_stack.Add(box);
+                    switch (to_stack)
+                    {
+                        case 1:
+                            lijst_1.Add(box);
+                            break;
+                        case 2:
+                            lijst_2.Add(box);
+                            break;
+                        case 3:
+                            lijst_3.Add(box);
+                            break;
+                        case 4:
+                            lijst_4.Add(box);
+                            break;
+                        case 5:
+                            lijst_5.Add(box);
+                            break;
+                        case 6:
+                            lijst_6.Add(box);
+                            break;
+                        case 7:
+                            lijst_7.Add(box);
+                            break;
+                        case 8:
+                            lijst_8.Add(box);
+                            break;
+                        case 9:
+                            lijst_9.Add(box);
+                            break;
+                        default:
+                            break;
+                    }
+
+
                 }
 
             }
